2de0ff18e05ecd44d14710bb6341d8135135a9de
Niv <daganniv@gmail.com>
2023-12-27 17:22:53 +0200

    Cookies, Header Added

--- main.py
+++ main.py
@@ -152,3 +152,19 @@ async def read_items(
         "start_process": start_process,
         "duration": duration,
     }
+
+@app.get("/items")
+async def read_items(
+    cookie_id: str | None = Cookie(None),
+    accept_encoding: str | None = Header(None),
+    sec_ch_ua: str | None = Header(None),
+    user_agent: str | None = Header(None),
+    x_token: list[str] | None = Header(None),
+):
+    return {
+        "cookie_id": cookie_id,
+        "Accept-Encoding": accept_encoding,
+        "sec-ch-ua": sec_ch_ua,
+        "User-Agent": user_agent,
+        "X-Token values": x_token,
+    }
