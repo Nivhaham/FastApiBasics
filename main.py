fc9585db743e7e1f63e3479d71604b0eef7090e6
Niv <daganniv@gmail.com>
2023-12-29 00:11:29 +0200

    easier to use

--- main.py
+++ main.py
@@ -275,22 +275,77 @@ def read_item(item_id: int):
     return {"item_id": item_id}
 
 
-# can extend functionality StarletteHTTPException or
+# can extend functionality sideeffects using StarletteHTTPException
 
-@app.exception_handler(StarletteHTTPException)
-async def custom_http_exception_handler(request, exc):
-    print(f"Omg! An HTTP error!: {repr(exc)}")
-    return await http_exception_handler(request, exc)
+# @app.exception_handler(StarletteHTTPException)
+# async def custom_http_exception_handler(request, exc):
+#     print(f"Omg! An HTTP error!: {repr(exc)}")
+#     return await http_exception_handler(request, exc)
+#
+#
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request, exc):
+#     print(f"OMG! The client sent invalid data!: {exc}")
+#     return await request_validation_exception_handler(request, exc)
+#
+#
+# @app.get("/blah_items/{item_id}")
+# async def read_items(item_id: int):
+#     if item_id == 10:
+#         raise HTTPException(status_code=418, detail="Nope! I don't like 10.")
+#     return {"item_id": item_id}
 
 
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    print(f"OMG! The client sent invalid data!: {exc}")
-    return await request_validation_exception_handler(request, exc)
+# tags is a way to handle routes in a mangeable manner.
+# example of how to explain the user's of the api how to use it better:
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
 
 
-@app.get("/blah_items/{item_id}")
-async def read_items(item_id: int):
-    if item_id == 10:
-        raise HTTPException(status_code=418, detail="Nope! I don't like 10.")
-    return {"item_id": item_id}
+class Tags(Enum):
+    items = "items"
+    users = "users"
+
+
+@app.post(
+    "/items/",
+    response_model=Item,
+    status_code=status.HTTP_201_CREATED,
+    tags=[Tags.items],
+    summary="Create an Item-type item",
+    # description="Create an item with all the information: "
+    # "name; description; price; tax; and a set of "
+    # "unique tags",
+    response_description="The created item",
+)
+async def create_item(item: Item):
+    """
+    Create an item with all the information:
+
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    """
+    return item
+
+
+@app.get("/items/", tags=[Tags.items])
+async def read_items():
+    return [{"name": "Foo", "price": 42}]
+
+
+@app.get("/users/", tags=[Tags.users])
+async def read_users():
+    return [{"username": "PhoebeBuffay"}]
+
+
+@app.get("/elements/", tags=[Tags.items], deprecated=True)
+async def read_elements():
+    return [{"item_id": "Foo"}]
